62990b47afbf98f573e5ddd2b15596704fdfca0b
tetsup <58599705+tetsup@users.noreply.github.com>
2026-05-28 23:30:15 +0900

    ci: test case changed for valid resource type

--- packages/schema/tests/common.test.ts
+++ packages/schema/tests/common.test.ts
@@ -2,12 +2,12 @@ import { IdSchema } from '@schema/resource/common/base';
 
 describe('IdSchema', () => {
   it('正常系', () => {
-    expect(IdSchema.parse('test/variable/status.poison')).toBe('test/variable/status.poison');
+    expect(IdSchema.parse('test/skin/player.hero')).toBe('test/skin/player.hero');
   });
 
   it('異常系', () => {
-    expect(() => IdSchema.parse('test/variable/.invalid')).toThrow();
-    expect(() => IdSchema.parse('test/variable/invalid.')).toThrow();
-    expect(() => IdSchema.parse('test/variable/a..b')).toThrow();
+    expect(() => IdSchema.parse('test/skin/.invalid')).toThrow();
+    expect(() => IdSchema.parse('test/skin/invalid.')).toThrow();
+    expect(() => IdSchema.parse('test/skin/a..b')).toThrow();
   });
 });
