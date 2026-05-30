08778aa8f35d974e0d9cbf2f9f8d54c503e89c40
tetsup <58599705+tetsup@users.noreply.github.com>
2026-05-30 12:00:04 +0900

    ci: use tsc --noEmit instead of -b

--- apps/api/src/utils/handle.ts
+++ apps/api/src/utils/handle.ts
@@ -4,9 +4,10 @@ import { ApiError } from '../errors/http-error';
 export function handle<T>(fn: (c: Context) => Promise<T>) {
   return async (c: Context) => {
     try {
+      console.log(c);
       const data = await fn(c);
       console.log(data);
-      return c.json(data);
+      return c.json(data ?? {}, c.req.method === 'POST' ? 201 : 200);
     } catch (err) {
       if (err instanceof ApiError) {
         return c.json(
