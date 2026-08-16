4133cc72e57392ef475993d08fe0f27692be8c36
tetsup <58599705+tetsup@users.noreply.github.com>
2026-08-16 12:51:42 +0900

    ops: add hono logger

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 import { cors } from 'hono/cors';
 import { resolveUserMiddleware } from './auth/middlewares/resolve-user';
 import { authRoute } from './routes/auth';
@@ -8,6 +9,7 @@ import { createCorsOriginValidator } from './utils/frontend-origin';
 
 const app = new Hono();
 
+app.use(logger());
 app.use(
   '*',
   cors({
