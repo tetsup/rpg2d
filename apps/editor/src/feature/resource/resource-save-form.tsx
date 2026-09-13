d1af18aecf50679eb7ddb3401a7f3193eee98d78
tetsup <58599705+tetsup@users.noreply.github.com>
2026-09-13 16:38:53 +0900

    refactor: keep DRY resource common form

--- apps/editor/src/feature/resource/resource-save-form.tsx
+++ apps/editor/src/feature/resource/resource-save-form.tsx
@@ -1,9 +1,8 @@
 import type { ReactNode } from 'react';
-import { useTranslation } from 'react-i18next';
 import type { UseFormReturn } from 'react-hook-form';
 import type { ResourceInput } from '@sharedTypes/database/collection';
-import { TextField } from '@editor/widget/field/text-field';
 import { FormShell } from '@editor/widget/shell/form-shell';
+import { ResourceCommonSection } from './resource-common-section';
 
 type ResourceSaveFormProps<T extends ResourceInput<any>> = {
   form: UseFormReturn<T>;
@@ -12,12 +11,9 @@ type ResourceSaveFormProps<T extends ResourceInput<any>> = {
 };
 
 export function ResourceSaveForm({ form, onSubmit, additionalField }: ResourceSaveFormProps<any>) {
-  const { t } = useTranslation();
   return (
     <FormShell form={form} onSubmit={onSubmit}>
-      <TextField name="namespace" label={t('グループ')} />
-      <TextField name="name" label={t('イメージ名')} />
-      <TextField name="description" label={t('説明')} />
+      <ResourceCommonSection />
       {additionalField}
     </FormShell>
   );
